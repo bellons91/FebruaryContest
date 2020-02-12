be879dbe82a53a9c5c0cdadf07e5ff849114069f
Davide Bellone <davide.bellone>
2020-02-12 13:59:26 +0100

    Created solution

--- FebruaryContestEntry/Program.cs
+++ FebruaryContestEntry/Program.cs
@@ -2,19 +2,54 @@
 
 namespace FebruaryContestEntry
 {
-	class Program
-	{
-		const char block = '▓';
+    class Program
+    {
+        const char block = '▓';
 
-		static void Main(string[] args)
-		{
-			// The solution implementation goes here
+        static void Main(string[] args)
+        {
+            // The solution implementation goes here
 
-			Console.ResetColor();
-			Console.WriteLine();
-			Console.WriteLine();
-			Console.WriteLine("Yay, a colorful shape!");
-			Console.ReadLine();
-		}
-	}
+            PrintSquare();
+
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Yay, a colorful shape!");
+            Console.ReadLine();
+        }
+
+        static void PrintSquare()
+        {
+            int maxRows = 15;
+            int maxCols = 30;
+
+            for (int row = 0; row < maxRows; row++)
+            {
+                for (int col = 0; col < maxCols; col++)
+                {
+                    if (col == 0 || row == 0 || col == (maxCols - 1) || row == (maxRows - 1))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(block);
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write(block);
+                        Console.ResetColor();
+                    }
+
+                    if (col == maxCols - 1)
+                        Console.WriteLine();
+
+                }
+
+            }
+
+        }
+    }
+
+
 }
